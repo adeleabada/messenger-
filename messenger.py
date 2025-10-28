ed31def4f85ecd5339b68c361025f57f8e3343aa
Adèle Abada <adele.abada@etu.minesparis.psl.eu>
2025-10-28 15:58:12 +0100

    affichage amelioré+channels

--- messenger.py
+++ messenger.py
@@ -19,12 +19,24 @@ server = {
     ]
 }
 users= server['users']
+channels= server['channels']
 print('=== Messenger ===')
+print( '1. see users')
+print('2. see channels')
 print('x. Leave')
+
 choice = input('Select an option: ')
+
 if choice == 'x':
     print('Bye!')
-elif choice =='u':
-    print(users)
+elif choice =='1':
+    for user in users:
+        print (user['id'], user['name']) 
+    print ('n.create user')
+    print ('x.Main menu')
+    choice1 = input('Enter a choice and press ENTER: ')           
+elif choice=='2':
+    for channel in channels:
+        print(channel['id'], channel['name'])
 else:
     print('Unknown option:', choice)
