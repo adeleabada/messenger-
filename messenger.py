2dbdd9824975171ad8f360ea0a1a6cf7eda55796
Adèle Abada <adele.abada@etu.minesparis.psl.eu>
2025-11-04 15:16:09 +0100

    creer new user, new groupe presque

--- messenger.py
+++ messenger.py
@@ -22,49 +22,99 @@ users= server['users']
 channels= server['channels']
 messages= server['messages']
 
+
+def id_name(nom)
+    for user in users:
+        if nom==user:
+            idnom=user['id']
+            return idnom
+
+
 def menu():
     print('=== Messenger ===')
     print( '1. see users')
     print('2. see channels')
     print('x. Leave')
+    choice = input('Select an option: ')
+    if choice == 'x':
+        print('Bye!')
+    
+    elif choice =='1':
+        user()
 
-print('=== Messenger ===')
-print( '1. see users')
-print('2. see channels')
-print('x. Leave')
+    elif choice=='2':
+        channel()
+    else:
+        print('Unknown option:', choice)
 
-choice = input('Select an option: ')
+
+def user():
+    for user in users:
+        print (user['id'], user['name']) 
+    print ('n.create user')
+    print ('x.Main menu')
+    choice1 = input('Enter a choice and press ENTER: ')
+    if choice1 =='x':
+          menu()
+    if choice1=='n':
+        name=input('Name: ')
+        user_ids=[]
+        for user in users:
+            user_ids.append(user['id'])
+        newid= max(user_ids)+1
+        users.append ( {'id':newid,'name':name })
+        print(users)
+def newgroup():
+    groupname= input ('Group Name')
+    print (users)
+    id_menbers=input('Id du membre')
+    for ids in id_menbers:
+        id_menbers.append(id_name(menber))
+
+    channel_ids=[]
+    for channel in channels:
+        channel_ids.append(channels['id'])
+    newgroup_id= max(channel_ids)+1
+    channels.append ( {'id':newgroup_id,'name':groupname,})
+    print(channels)
+     
+
+
+
+def channel():
+    for channel in channels:
+        print(channel['id'], channel['name'])
+        print("1.choose group")
+        print("2.New group")
+        print("x. menu ")
+        choice2 = input('Enter a choice and press ENTER: ')
+    if choice2=='1':  
+       choice22=int(input("group number"))
+       for message in messages:
+            if choice22 == message['channel']:
+                print(message['sender_id'],message['content'])
+            else:  
+            print ("no group")
+    elif choice2== 'x':
+        menu()
+    elif choice2 == '2':
+        newgroup()
+        
+
+
+    
+
+
+
+menu()
 
 if choice == 'x':
     print('Bye!')
     
 elif choice =='1':
-
-    for user in users:
-        print (user['id'], user['name']) 
-        print ('n.create user')
-        print ('x.Main menu')
-        choice1 = input('Enter a choice and press ENTER: ')
-        identité=[]
-        identite=identite.append(user['id'])
-    if choice1='n'
-        name= input('Name: ')
-        id=
-        if id in identité
+    user()
 
 elif choice=='2':
-
-    for channel in channels:
-        print(channel['id'], channel['name'])
-    choice2 = int((input('choose group: , for menu enter 999 ')))
-    for message in messages:
-        if choice2 == message['channel']:
-            print(message['sender_id'],message['content'])
-
-        elif choice2== 999:
-            menu()
-        else:  
-
-            print ("no group")
+    channel()
 else:
     print('Unknown option:', choice)
